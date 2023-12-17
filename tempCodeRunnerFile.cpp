2e89c23dd2725ac0b9b04e3563f731318c8581de
haikalmsg <117971970+haikalmsg@users.noreply.github.com>
2023-12-17 11:37:58 +0700

    changed filenames

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,14 @@
-static_cast<int>(
+int mod_pow(int base, int exp, int mod) {
+    int result = 1;
+    base = base % mod;
+
+    while (exp > 0) {
+        if (exp % 2 == 1)
+            result = (result * base) % mod;
+
+        exp = exp >> 1;
+        base = (base * base) % mod;
+    }
+
+    return result;
+}
